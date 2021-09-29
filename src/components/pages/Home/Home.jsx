45116df3ef49293245cbdbff0ff59772ec0e31a1
DiliniNanayakkara <dlnnimesha@gmail.com>
2021-09-29 22:45:15 +0530

    Home page

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
         
     },[]);
     return (
-       <React.Fragment>
+      <div>
            <NavBar />
        
                                 
@@ -68,17 +68,204 @@ there in a “good” versus “evil” battle, reminiscent of the plots of the 
 <p>Video games are good for helping children develop problem-solving skills. They aren’t lazy as previous research has suggested. 
 Video games can strengthen your child’s cognitive skills. They can improve your child’s capacity to think in
  three-dimensional ways. </p>
+ <h3 className="hed">Ethical Issues of Video Games for Kids</h3>
+ <img className="imgg" src="https://www.parhlo.com/wp-content/uploads/2019/11/video-game-addiction.jpg.webp"/>
+ <p>According to Dang et al. (2007) new video games are ethically affecting people who play them. 
+     Additionally, the ethical issues include: violence, rating, education, stereotyping against women, 
+     community and addiction. computer games can be a great recreational hobby. However, video game addiction can lead 
+     to several issues. The negative effects
+      of Computer games include effects on our mind, 
+     body relationships, job performance, and more.
+
+It is important to remember that Cumputer games are not inherently evil. Most of the negative effects of Cumputer games arise from 
+excessive use and addiction. Here are ten negative effects of Computer games</p>
+<h5 className="subhed">01. Dopamine Addiction</h5>
+<img className="imgg" src="https://assets.website-files.com/60807ca2d01082168c9aac03/60a9d93df4bfe1629fde9391_Brain-Dopamine-Addiction-1024x576.jpeg"/>
+<p>The reward circuitry in your brain is one of the reasons video games are fun. Your brain releases a neurotransmitter 
+    called dopamine, which regulates the feeling of pleasure from video games. When dopamine activity happens in the nucleus accumbens, 
+    the pleasure center of your brains, you feel joy.
+
+When you play video games, your brain releases a constant dopamine supply, with occasional random bursts. Over time, your brain gets 
+used to this steady supply of dopamine, and the nucleus accumbens requires even more dopamine release for gaming to feel fun. That 
+prevents you from enjoying anything that is not as stimulating as a video game.
+
+Even if you try to give up gaming, your mind will attract you to other forms of stimulation, such as browsing YouTube or scrolling 
+through social media. Simple activities will seem completely unappealing, and you will not be motivated to do anything that isn’t 
+exciting. That is the height of dopamine addiction. If you let your gaming habit get out of control, this is a real possibility.</p>
+
+
+
+<h5 className="subhed">Negative Effects of Computer Games on Motivation</h5>
+<img className="imgg" src="https://images.saymedia-content.com/.image/t_share/MTc0NDYyMjk1ODAyMzg5ODY0/the-dark-side-of-social-media.jpg"/>
+<p>Unlike the nucleus accumbens, we cannot map the triumph circuit onto the brain as easily. It does not localize to one part of the brain — it’s just a general pattern of psychology and cognition integral to human beings.
+
+The triumph circuit makes us feel good when we overcome a challenge. In evolutionary biology, there’s a strong story about 
+venturing into the unknown, finding something valuable, overcoming some kind of problem, and then bringing it back to society.
+ That has shaped social psychology — we as human beings value people who overcome odds.
+
+Video games have hacked this circuit. They have found a way to deliver all the psychological rewards that we often seek to gain 
+from the real world. What we want from video games is often very similar to what we want from life. We want excitement, adventures, 
+friendships, victories, and status. Games have found a way to serve that to us with little effort on our end. As a result, a person’s 
+ability to engage the triumph circuit 
+suffers when they can just engage it with video games.</p>
+
+
+
+<h5 className="subhed">Emotions</h5>
+<img className="imgx" src="https://www.claremontportside.com/wp-content/uploads/2020/09/images4545-5f6cb35393213.jpg"/>
+<p>When we experience fear, worry, shame, or other negative emotions, the amygdala becomes active. The amygdala is the part of that 
+    brain that governs these negative emotions. It is also integral to our learning circuitry since it creates connections to the 
+    hippocampus, the center for learning in our brain.
+
+However, fMRI studies have shown that if we start to play video games when our amygdala is active, it calms down. The effects of 
+this are two-fold:
+
+Constant suppression of negative emotions can lead to a condition called alexithymia. If a person is alexithymic, then that means that 
+they have trouble determining their internal emotional state. Alexithymic people find it hard to pinpoint the emotions that they are 
+feeling. However, that does not mean that those emotions do not affect their behavior.
+The amygdala is a core part of our learning circuitry. Painful experiences get imprinted onto our memory, and we learn from them. 
+However, people find it hard to quit playing video games because their amygdala does not make connections with their hippocampus as 
+smoothly. They don’t learn from the painful experiences of not doing well in school or at work. Their learning circuitry gets sabotaged, 
+and it becomes difficult 
+for them to quit gaming.</p>
+
+<h5 className="subhed">Health Effects</h5>
+<img className="im" src="https://www.daimanuel.com/wp-content/uploads/2018/10/isolation-from-reality-gaming-addiction.png"/>
+<p>Playing video games is not an unhealthy activity on its own. However, if done for hours on end, it can quickly become detrimental 
+    to one’s physical health. 
+    Here are some health issues that can arise due to video game addiction.</p>
+    <h6>Repetitive Stress Injuries</h6>
+    <p>Repetitive Stress Injuries (RSIs) are relatively common among gamers. Gaming requires strenuous, 
+        repetitive motion on the hands and wrists (sometimes for hours on end). These motions can cause inflammation and 
+        injury. Here are some RSIs that you can develop from gaming:
+
+Gamer’s thumb is a nickname given to a condition called de Quervain’s Tenosynovitis. It is a condition in which the tendons that move
+ our thumb become inflamed, which leads to pain and limited movement.
+Trigger finger, also known as Stenosing Tenosynovitis, is a condition in which a finger gets stuck in a bent position. It may bend or 
+straighten with a snap, like a trigger being pulled or released, hence the name.
+Tennis elbow (Lateral Epicondylitis) is also a potential RSI that you can develop by gaming too much. It is a painful condition in
+ which the tendons in the elbow get overloaded, usually due to repetitive movement of the wrist and arms.
+Carpal Tunnel Syndrome is another RSI that can be caused by gaming too much. Its characteristic symptoms are numbness, tingling,
+ or weakness in the hand. It occurs due to pressure on the median nerve. This nerve runs from your forearm into your hand through a
+  part of your wrist called the carpal tunnel.</p>
+
+<h5 className="subhed">Other Physical Health Issues</h5>
+<img className="imgg" src="https://osu-wams-blogs-uploads.s3.amazonaws.com/blogs.dir/2098/files/2019/08/physical-health-problem-header-690px.jpg"/>
+<p>We all know the feeling of getting lost for hours in a game, often forgetting to eat, or snacking on unhealthy foods. It should not be a surprise then, that obesity and malnutrition are risk factors from gaming too much. There is also some preliminary evidence that excessively playing video games can lead to obesity in teens.
+Eyestrain is a pervasive issue among gamers. Staring at a screen for hours on end. The eyes focus much differently on a video screen 
+than any other flat surface, such as a notebook. It is less strenuous to stare at a notebook because the distance is constant, and the 
+eyes know where to focus. However, on a video screen, the focus is continually changing, leading to eye fatigue. It becomes difficult 
+for the eyes to focus on other objects, even after 
+the gaming session is over.
+A study done on Australian adolescents found that prolonged periods of sitting at a computer could alter their developing 
+neuromusculoskeletal system. Posture problems are incredibly common among gamers. The body tries to obtain the least energy-demanding 
+position when it finds itself in one spot for prolonged periods. As a result, rounded shoulders, head leaned forward, a curved lower 
+back, and back and neck problems later in life are common among gamers.</p>
+
+<h5 className="subhed">Affect the Brain Negatively</h5>
+<img className="i" src="https://assets.website-files.com/60807ca2d01082168c9aac03/60a9d93df18a35901e0e7308_Depression-1024x683.jpeg"/>
+<p>Video games do not necessarily lead to mental health problems. However, you can be at high risk for video game addiction if you 
+    already suffer from depression, anxiety, or other mental illness. This is due to the way video games affect your brain.
+
+A study that looked at the effect of gaming on mental health found that problematic gaming habits correlated with maladaptive coping 
+strategies, negative emotions, low self-esteem, a preference for solitude, and poor school performance.
+
+While one of the benefits of gaming is an increase in the ability to concentrate, excessive gaming may lead to the opposite. A study 
+done on 3000 children and adolescents from 12 different schools in Singapore found a bidirectional causality in play between video 
+games and attention deficit disorders. While excessive gaming could contribute to a lack of long-term concentration ability, it is 
+also likely that children who already
+ show signs of attention deficit disorders would be more attracted to video games, due to the variety of experiences they can provide.</p>
+
+<h5 className="subhed">Relationship Issues</h5>
+<img className="i" src="https://assets.website-files.com/60807ca2d01082168c9aac03/60a9d93df39184aec9bbacf1_Family-1024x683.jpeg"/>
+<p>Relationship issues resulting from playing too many video games is a common issue among gamers and their families. When people get 
+    too addicted to video games, they start to neglect other parts of their lives. The first parts to go are wholesome and 
+    supportive relationships with family, friends, and loved ones.
+
+Relationships need work to maintain. They are like a plant — if you neglect one long enough, it will wither up and die. Relationships 
+need to regular nurturing for them to grow or even maintain. However, when a person spends most of their time playing video games, 
+there is almost no time left in the day to talk to friends and family, let alone pursue romantic relationships.
+
+A healthy amount of gaming with friends and family can lead to stronger bonds. However, excessive gaming often results in isolation. 
+Therefore, it is much more important to keep an eye on your relationships and see if they are moving down a path that you do not desire.</p>
+
+<h5 className="subhed">Social Disconnection</h5>
+<img className="imgg" src="https://assets.website-files.com/60807ca2d01082168c9aac03/60a9d93e492f9d6ace30ed7c_video-games-and-depression-1024x684.jpeg"/>
+<p>Social disconnection is a real problem in the gaming community. Out of all the negative effects of video games, this one is 
+    touted a lot. Though we play multiplayer games, we rarely end up connecting with the people in our games. Some communities are 
+    more conducive to authentic human interaction than others. However, those are the exception. Isolation can be extremely detrimental 
+    to your mental health. Evolution did not design human beings to be solitary creatures. We thrive in groups where can we can support 
+    each other.
+
+An Iranian study done on second-grade students in 2009 found that individuals who were addicted to video games tended to have lower 
+social skills than those who were not. Social interaction is a skill, and if you spend most of your time alone in your room, playing 
+games, and not interacting with other people in real life, then that skill will deteriorate. That can lead to social anxiety, which
+ makes the problem worse.
+
+</p>
+
+<h5 className="subhed">
+Toxic Environments</h5>
+<img className="imgg" src="https://assets.website-files.com/60807ca2d01082168c9aac03/60a9d93ed93f7dc5ac58440c_Burning-Environment-1024x683.jpeg"/>
+<p>In our society, men are culturally only allowed to express one negative emotion: anger. If they express other negative emotions, such as shame or fear, they are seen as weak or overly sensitive. 
+    That makes men more susceptible to developing alexithymia, especially if they are addicted to video games.
+
+Statistics on the gender breakdown on the internet say that it consists of about 54% males and 46% females, as of 2019. However, some 
+spaces such as Twitch are male-dominated. That is a lot of people in the gaming community who might feel that anger is the only 
+acceptable emotion they can express. Hence, it is no surprise that these communities are so toxic.
+
+Some gaming communities are more toxic than others. A potential reason for that could be that some games are more competitive than 
+others, and competition sometimes does not bring out the best in us.</p>
+
+<h5 className="subhed">Academic Performance</h5>
+<img className="imgg" src="https://assets.website-files.com/60807ca2d01082168c9aac03/60a9d93eb72ec30874c5888f_Failure-1024x538.jpeg"/>
+<p>If you are addicted to gaming, then your academic and professional success is likely to suffer. People who play games excessively 
+    find it challenging to pay attention to their jobs or school. It is quite common to turn to video games when your performance in 
+    these areas suffers. Games turn into an escape when the responsibilities in your life feel overwhelming.
+
+A cross-sectional and longitudinal study of adolescents in secondary school showed a negative association of screen time with school 
+achievement one year later, independent of their physical activity, age, gender, and socio-economic status. It suggests a link between 
+increased gaming and poor academic performance.
+
+You can have a healthy gaming habit and still maintain excellent performance at school or work. But due to the way games affect 
+your brain, it is much easier to lose yourself in the virtual world then to try to tackle hardships in the real world. For example, many children and teenagers start gaming because they find a community that does not judge them for their socio-economic background or physical appearance. They get attached to this community and start
+ to become disinterested in their performance in school, which leads to their grades taking a hit.</p>
+
+<h5 className="subhed">Escapism and Being Stuck in Life</h5>
+<img className="imgg" src="https://scandasia.com/wp-content/uploads/2021/09/Playing-games.jpg"/>
+<p>We know that video games provide a safe space for people to escape to when they do not feel equipped to deal with the problems
+
+    in their life. However, sometimes this can become problematic. If video games prevent you from finding ways to deal with your issues, then that is an issue.
+
+If you spend 10 hours a day playing video games, then there is a chance that you are not facing the problems in your life head-on. While it can be quite painful to do so, it is also one of the best ways to move forward. You don’t have to quit video games. You can maintain a healthy balance by taking one step at a time to take back
+ control of your life.</p>
+
+<h5 className="subhed">Conclusion</h5>
+
+<p>As you might have noticed, most of the negative effects of video games arise from excessive use. Even the health issues mentioned 
+    only become a problem when a person spends hours in one place playing video games.
+
+While it may seem like video games can create a lot of problems, there are plenty of gamers that lead successful lives and still play 
+video games. Moderate doses of gaming make all the difference between playing games 
+for enjoyment and gaming controlling your life. If you can address the underlying issues that make you want to play video games for 10+ 
+hours a day, you can take back control of your life.
+
+‍
+
+If you feel that your gaming habit is affecting your life, we can help. Sign up to work with a Healthy Gamer Coach, trained by Dr. 
+Alok Kanojia himself. Healthy Gamer Coaches are gamers who have taken control of their life, and know exactly what you’re going through.</p>
+
 </div>
              </div>         
 
        </div>
           
-                <Footer/>
-             </React.Fragment>  
+              
+           
              
       
              
-            
+     </div>       
        
     )
 }
